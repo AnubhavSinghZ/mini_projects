093f160ee33d83d4567055eba5e14427fbb0c89b
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-24 21:45:21 +0530

    Add files

--- expence_tracker.py
+++ expence_tracker.py
@@ -1,3 +1,9 @@
 # Expence Tracker 
 
-expences=[]  # list of all expences
+expences=[]  # list of expences in form of dictionary
+print("===Welcome To Expence Tracker:: Save Money Bro")
+while True:
+    print("-- MENU --")
+    print("1. Add Expence")
+    print("2. View All Expence")
+    print("3. Exit")
