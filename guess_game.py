1fc4d9080a97bd562185cd240cd43ddfcb7878ac
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-26 11:38:21 +0530

    completed a mini project

--- guess_game.py
+++ guess_game.py
@@ -9,7 +9,43 @@ word=random.choice(words) #Randomly choose a word from the list
 print("\n Guess the character in the word")
 
 #  Store guessed characters
-guess=""
+guesses=""
 
 # Number of turns
 Turns= 12
+
+while Turns>0:
+    failed=0
+
+    for char in word:
+        if char in guesses:
+            print(char, end="")
+        else:
+            print("_", end="")
+            failed+=1
+    print()
+
+    if failed==0:
+        print("You Win")
+        print("The word is:", word)
+        break
+    guess=input("Guess a character:").lower()
+
+    if len(guess)!=1:
+        print("Please enter a single character:")
+        continue
+
+    if guess in guesses:
+        print("You have already guessed it.")
+        continue
+
+    guesses+=guess
+
+    if guess not in word:
+        Turns-=1
+        print("wrong")
+        print("You have", Turns,"more guesse")
+
+        if Turns==0:
+            print("You Lose.")
+            print("The last word was:", word)
