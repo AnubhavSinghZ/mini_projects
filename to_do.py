68a87b98935bff24c333a312a092e60950ac824a
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-18 23:13:59 +0530

    adding syntax

--- to_do.py
+++ to_do.py
@@ -5,4 +5,11 @@ def extract_intent(sentence):
     sentence=sentence.lower()
 
     #--- Intent Detection (String Parsing)----
-    
+    if any(w in sentence for w in["remind me to", "add", "i need to"]):
+        intent="add"
+    elif any(w in sentence for w in["done", "finished", "completed"]):
+        intent="done"
+    elif any(w in sentence for w in["delete", "remove", "cancel"]):
+        intent="delete"
+    else:
+        intent="unknown"
